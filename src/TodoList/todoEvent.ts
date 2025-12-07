ade2b68e3ce84bcf097f3b6bcef55d14fa536ab8
JeromeJoh <jerome.yzhao@foxmail.com>
2025-12-07 22:21:31 +0800

    fix: unused parameters add underscore

--- src/TodoList/todoEvent.ts
+++ src/TodoList/todoEvent.ts
@@ -1,95 +1,93 @@
-import { ITodo } from '.';
-import todoItem, { emptyTip } from './template';
+import { ITodo } from '.'
+import todoItem, { emptyTip } from './template'
 
-let todoData: ITodo[];
-let oTodoWrapper: HTMLElement;
-const emptyTipString: string = emptyTip('No tasks in your list now !');
+let todoData: ITodo[]
+let oTodoWrapper: HTMLElement
+const emptyTipString: string = emptyTip('No tasks in your list now !')
 
 function saveToLocal(): void {
-  localStorage.setItem('TodoAppData', JSON.stringify(todoData));
+  localStorage.setItem('TodoAppData', JSON.stringify(todoData))
 }
 
 export function renderList(
-  target: any,
-  methodName: string,
+  _target: any,
+  _methodName: string,
   descriptor: PropertyDescriptor
 ): void {
-  const _origin = descriptor.value;
-  todoData = JSON.parse(localStorage.getItem('TodoAppData') || '[]');
+  const _origin = descriptor.value
+  todoData = JSON.parse(localStorage.getItem('TodoAppData') || '[]')
 
   descriptor.value = function (oTodoList: HTMLElement) {
-    _origin.call(this, oTodoList);
+    _origin.call(this, oTodoList)
 
-    oTodoWrapper = oTodoList;
+    oTodoWrapper = oTodoList
 
     if (todoData.length === 0) {
-      oTodoList.innerHTML = emptyTipString;
-      return;
+      oTodoList.innerHTML = emptyTipString
+      return
     }
 
-    const oFragment = document.createDocumentFragment();
-    todoData.map((todoDatum) => oFragment.appendChild(todoItem(todoDatum)));
-    oTodoList.appendChild(oFragment);
-  };
+    const oFragment = document.createDocumentFragment()
+    todoData.map((todoDatum) => oFragment.appendChild(todoItem(todoDatum)))
+    oTodoList.appendChild(oFragment)
+  }
 }
 
 export function addTodo(
-  target: any,
-  methodName: string,
+  _target: any,
+  _methodName: string,
   descriptor: PropertyDescriptor
 ): void {
-  const _origin = descriptor.value;
+  const _origin = descriptor.value
 
   descriptor.value = function (todo: ITodo) {
-    const _item: ITodo | null = todoData.find(
-      (t) => t.content === todo.content
-    );
+    const _item: ITodo | null = todoData.find((t) => t.content === todo.content)
 
     if (_item) {
-      alert('请勿重复创建任务');
-      return;
+      alert('请勿重复创建任务')
+      return
     }
 
-    todoData.push(todo);
-    saveToLocal();
+    todoData.push(todo)
+    saveToLocal()
 
-    if (todoData.length === 1) oTodoWrapper.innerHTML = '';
+    if (todoData.length === 1) oTodoWrapper.innerHTML = ''
 
-    _origin.call(this, todo);
-  };
+    _origin.call(this, todo)
+  }
 }
 
 export function removeTodo(
-  target: any,
-  methodName: string,
+  _target: any,
+  _methodName: string,
   descriptor: PropertyDescriptor
 ): void {
-  const _origin = descriptor.value;
+  const _origin = descriptor.value
 
   descriptor.value = function (id: number) {
-    todoData = todoData.filter((todo) => todo.id !== id);
-    saveToLocal();
+    todoData = todoData.filter((todo) => todo.id !== id)
+    saveToLocal()
 
-    if (todoData.length === 0) oTodoWrapper.innerHTML = emptyTipString;
+    if (todoData.length === 0) oTodoWrapper.innerHTML = emptyTipString
 
-    _origin.call(this, id);
-  };
+    _origin.call(this, id)
+  }
 }
 
 export function changeCompleted(
-  target: any,
-  methodName: string,
+  _target: any,
+  _methodName: string,
   descriptor: PropertyDescriptor
 ): void {
-  const _origin = descriptor.value;
+  const _origin = descriptor.value
 
   descriptor.value = function (id: number) {
     todoData = todoData.map((todo) => {
-      if (todo.id === id) todo.completed = !todo.completed;
-      return todo;
-    });
-    saveToLocal();
+      if (todo.id === id) todo.completed = !todo.completed
+      return todo
+    })
+    saveToLocal()
 
-    _origin.call(this, id);
-  };
+    _origin.call(this, id)
+  }
 }
